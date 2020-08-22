fdad1e78fa27c62c59d47a014298ffa5749cd7c8
Adrian2256 <adrian.225651@gmail.com>
2020-08-22 18:53:27 +0200

    Game Audio Improvements

--- Assets/Audio/CharacterAudio.cs
+++ Assets/Audio/CharacterAudio.cs
@@ -7,27 +7,50 @@ using UnityEngine.InputSystem;
 
 public class CharacterAudio : MonoBehaviour
 {
+    [Range(0, 1)]
+    public float Volume = 0;
 
     public GameObject character;
     public GameObject footsteps;
+    public GameObject footstepsLow;
     public GameObject cut;
 
     public float footstepsSpeed = 0.5f;
+    [Range(0, 1)]
+    public float Grass_or_Stone = 0;
+
     private float nextFootstep;
+    public int feet = 0;
 
     private void Awake()
     {
         nextFootstep = 0;
     }
     //Functions
+    private void Update()
+    {
+        footsteps.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("FootstepVolume", Volume);
+        footstepsLow.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("FootstepVolume", Volume);
+        cut.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("CutVolume", Volume);
+        footsteps.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("Grass_Stone", Grass_or_Stone);
+        footstepsLow.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("Grass_Stone", Grass_or_Stone);
 
+    }
     //Sounds
     public void FootStep()
     {
         if (Time.time > nextFootstep)
         {
             nextFootstep = Time.time + footstepsSpeed;
-            footsteps.GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            if(feet == 0)
+            {
+                footsteps.GetComponent<FMODUnity.StudioEventEmitter>().Play();
+                feet = 1;
+            } else
+            {
+                footstepsLow.GetComponent<FMODUnity.StudioEventEmitter>().Play();
+                feet = 0;
+            }
         }
     }
     public void Cut()
---
+++ Assets/GameAudio.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameAudio : MonoBehaviour
+{
+    public Slider ambienceSlider;
+    public Slider musicSlider;
+    public Slider SFXSlider;
+
+    public FMODUnity.StudioEventEmitter ambience;
+    public FMODUnity.StudioEventEmitter music;
+
+    public FMODUnity.StudioGlobalParameterTrigger reverbTime;
+    public FMODUnity.StudioGlobalParameterTrigger reverbWet;
+
+    private GameObject player;
+    private GameObject[] plants;
+
+    public float ReverbTime = 0;
+    public float ReverbWet = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ambience.Play();
+        music.Play();
+
+        player = GameObject.FindWithTag("Player");
+        plants = GameObject.FindGameObjectsWithTag("Plant");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.SetParameter("Volume", musicSlider.value);
+        ambience.SetParameter("Volume", ambienceSlider.value);
+
+        foreach(GameObject go in plants)
+        {
+            go.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("OverallVolume", SFXSlider.value);
+        }
+        player.GetComponentInChildren<CharacterAudio>().Volume = SFXSlider.value;
+
+        reverbTime.value = ReverbTime;
+        reverbWet.value = ReverbWet;
+    }
+}
