b2bfceaec78169c9fdf538e7db7b71a4de6f7f9e
Adrian2256 <adrian.225651@gmail.com>
2020-08-22 12:09:33 +0200

    Footstep SFX Working

--- Assets/Audio/CharacterAudio.cs
+++ Assets/Audio/CharacterAudio.cs
@@ -10,19 +10,22 @@ public class CharacterAudio : MonoBehaviour
     public GameObject cut;
 
     public float footstepsSpeed = 0.5f;
+    private Vector2 prevPos;
+    private Vector2 currentPos;
+    private float nextFootstep;
 
     void FixedUpdate()
     {
-        //If its moving, iniciate a sort of "cooldown" timer for the footstep SFX.
-        /*
-        if (Moving)
+        currentPos = character.transform.position;
+        if (prevPos != currentPos)
         {
-            //Cooldown timer
-            footsteps.GetComponent<FMODUnity.StudioEventEmitter>().Play();
-        } else
-        {
-            footsteps.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+            if (Time.time > nextFootstep)
+            {
+                nextFootstep = Time.time + footstepsSpeed;
+                footsteps.GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            }
         }
-        */
+        Debug.Log(character.transform.position);
+        prevPos = currentPos;
     }
 }
