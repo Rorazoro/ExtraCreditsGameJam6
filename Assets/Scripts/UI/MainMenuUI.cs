431d1aff6bb3cc506c4437060575f35d9d18ae3d
Rorazoro <rorazoro@gmail.com>
2020-08-23 21:45:21 -0400

    Hooked up main menu to game scene

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -4,7 +4,29 @@ using Assets.Managers;
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour {
-    public void PlayGame () {
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    // Start is called before the first frame update
+    void Start () {
+        MainMenu.SetActive (true);
+        SettingsMenu.SetActive (false);
+    }
+
+    public void PlayButton () {
         SceneLoader.Instance.LoadScene (Scenes.Prototype2, 1f, null);
+    }
+
+    public void SettingsButton () {
+        MainMenu.SetActive (false);
+        SettingsMenu.SetActive (true);
+    }
+
+    public void ExitSettingsButton () {
+        MainMenu.SetActive (true);
+        SettingsMenu.SetActive (false);
+    }
+
+    public void ExitButton () {
+
     }
 }
